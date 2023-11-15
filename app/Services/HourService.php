7e7f924c7e828e657e3cd6c561b66da8fe919f50
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-11-15 10:14:45 +0900

    保存処理に追加

--- app/Services/HourService.php
+++ app/Services/HourService.php
@@ -45,8 +45,8 @@ class HourService implements HourServiceInterface
         $input += ['user_id' => $request->user()->id];
         $input += ['total_cost' => $input['rent'] + $input['water_cost'] + $input['utilitiy_cost'] + $input['food_cost']
                                                                          + $input['phone_cost'] + $input['other_cost']];
-        $input += ['amount' => ceil($input['income'] - $input['total_cost']) / $input['hourly_wage']];
-        // $intInput = ceil($input['amount']);
+        $amount += ['amount' => ($input['income'] - $input['total_cost']) / $input['hourly_wage']];
+        $input = ceil($amount);
         $hour->fill($input)->save();
     }
     
