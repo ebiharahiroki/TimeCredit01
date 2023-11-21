8952ad04e49efb7f8d0ecb0aa995201f33a4628b
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-11-21 16:17:50 +0900

    機能テストクラス追加、phpunit.xmlファイルのテストに使うDB設定

---
+++ tests/Feature/HourControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HourControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_作成画面を表示できるか()
+    {
+        $user = User::factory()->create();
+ 
+        $response = $this->actingAs($user)->post('/post', [
+            'title' => 'テスト投稿',
+            'body' => 'テスト投稿をします',
+        ]);
+ 
+        $response->assertStatus(302);
+ 
+        $this->assertDatabaseHas('posts', [
+            'title' => 'テスト投稿',
+            'body' => 'テスト投稿をします',
+            'user_id' => $user->id,
+        ]);
+        
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)->post('/hours/create', [
+            
+        ]);
+        $this->get('/')->assertStatus(200);
+    }
+}
